71a0f0f581b79a4c64bc018b480e6c5a30520aea
Ксения Чемлекчиева <your_next_life@mail.ru>
2021-12-12 17:05:27 +0300

    Добавьте файлы проекта.

---
+++ Exercise2/Exercise2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exercise2
+{
+    class Exercise2
+    {
+        static void Main(string[] args)
+        {
+            int[,] A = new int[,] {
+                { 1, 1, 2, 4, 5 },
+                {-1, 3, 4, 0,-3 },
+                {-3,-8, 2, 5,-1 }
+            };
+            int maxColum = 0;
+            int maxPositive = 0;
+
+            for(int i=0; i<5; i++)
+            {
+                int k = 0;
+
+                for (int j=0; j<3; j++)
+                {
+                    Console.Write($"{A[j, i]}\t");
+                    if (A[j, i] > 0)
+                        k++;
+                }
+
+                if (k > maxPositive)
+                {
+                    maxPositive = k;
+                    maxColum = i;
+                }
+
+                Console.Write($"\t\tk={k}\n");             
+            }
+
+            Console.WriteLine($"{maxColum}");            
+        }
+    }
+}
---
+++ Lab6/Exercise1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lab6
+{
+    class Exercise1
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[] { 1, 2, 3, 4, 5 };
+            int[] B = new int[] { 3, 5, 6, 10, 10 };
+            int[] RES = new int[10];
+            int k = 5;
+
+            for(int i=0; i<5; i++)
+            {
+                RES[i] = A[i];
+            }
+
+            for(int i=0; i<5; i++)
+            {
+                int b = B[i];
+                bool inRes = false;
+
+                for(int j=0; j<9; j++)
+                {
+                    if (RES[j] == b)
+                        inRes = true;
+                }
+                if(!inRes)
+                {
+                    RES[k] = b;
+                    k++;
+                }
+            }
+            Console.WriteLine(k);
+
+            int[] C = new int[k];
+            for (int i = 0; i < k; i++)
+            {
+                C[i] = RES[i];
+            }
+
+            for (int i = 0; i < k; i++)
+            {
+                Console.WriteLine($"{C[i]}, ");
+            }
+                
+        }
+    }
+}
